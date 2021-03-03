caf7011ed05070c38e84f5dc919f0dda4c476fb3
yuangang <yuangang@limikeji.com>
2021-03-03 15:52:18 +0800

    format

--- app/src/main/java/com/example/androiddevchallenge/DogViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/DogViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.lifecycle.ViewModel
@@ -11,14 +26,14 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "母",
                 "上海",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103021648434984.jpg"
+                ""
             ),
             Dog(
                 "汤圆",
                 "小狗",
                 "公",
                 "南昌",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103021642417259.jpg"
+                ""
             ),
 
             Dog(
@@ -26,7 +41,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "母",
                 "北京",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103021651525447.jpg"
+                ""
             ),
 
             Dog(
@@ -34,7 +49,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "公",
                 "上海",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103020022398727.jpg"
+                ""
             ),
 
 
@@ -43,7 +58,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "母",
                 "南昌",
-                "https://img.chonganjia.cn/diary/adopt/202012/31/202012311700271509.jpg"
+                ""
             ),
 
 
@@ -52,7 +67,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "母",
                 "上海",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103021601025383.jpg"
+                ""
             ),
 
 
@@ -61,7 +76,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "母",
                 "北京",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103021528324951.jpg"
+                ""
             ),
 
             Dog(
@@ -69,7 +84,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "公",
                 "上海",
-                "https://img.chonganjia.cn/diary/adopt/202102/26/202102262106112199.jpg"
+                ""
             ),
 
             Dog(
@@ -77,7 +92,7 @@ class DogViewModel : ViewModel() {
                 "小狗",
                 "公",
                 "北京",
-                "https://img.chonganjia.cn/diary/adopt/202103/02/202103021212019049.jpg"
+                ""
             ),
         )
     }
